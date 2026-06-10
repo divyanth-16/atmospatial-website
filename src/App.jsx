168717915963de724d5395609059a6dd3153bce9
Divyanth-Penki <penkidivyanth75@gmail.com>
2026-06-10 18:27:02 +0530

    initial website

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import logo from "./assets/Atmospatial_Analytics_Pvt_Ltd_Transparent_logo.png";
+
+
+function App() {
+  
+
+  return (
+   <div className='flex flex-col items-center justify-center h-screen text-4xl font-bold bg-blue-100'>
+  <img
+    src={logo}
+    alt="Company Logo"
+    className="w-48 mb-4"
+  />
+  <h1>Website Under Construction...</h1>
+</div>
+  )
+}
+
+export default App
